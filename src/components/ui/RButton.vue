a7b6fae6435256a9f314066c4ef854cd8efcf360
Jakub Filo <jakub@filo.dev>
2021-12-20 16:30:22 +0100

    universal spinner component

--- src/components/ui/RButton.vue
+++ src/components/ui/RButton.vue
@@ -2,58 +2,27 @@
   <component
     :is="type"
     v-on="$listeners"
-    class="
-  inline-flex
- justify-center
- items-center
- border
- border-transparent
- text-base
- leading-6
- font-medium
- rounded-md
- active:bg-blue-700
- transition
- ease-in-out
- duration-150"
+    class="inline-flex items-center justify-center text-base font-medium leading-6 transition duration-150 ease-in-out border border-transparent rounded-md active:bg-blue-700"
     :class="btnClass"
     :loading="loading"
     :disabled="disabled"
     :size="size"
     :href="to"
   >
-    <svg
-      v-if="loading"
-      class="w-5 h-5 mr-3 -ml-1 text-white animate-spin"
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-    >
-      <circle
-        class="opacity-25"
-        cx="12"
-        cy="12"
-        r="10"
-        stroke="currentColor"
-        stroke-width="4"
-      ></circle>
-      <path
-        class="opacity-75"
-        fill="currentColor"
-        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-      ></path>
-    </svg>
+    <spinner v-if="loading" class="ml-1 mr-3" />
     <slot />
   </component>
 </template>
 
 <script>
 import Vue from 'vue'
+import Spinner from './Spinner.vue'
 
 const sizes = ['small', 'normal', 'full']
 const variants = ['primary', 'secondary', 'danger']
 
 export default Vue.extend({
+  components: { Spinner },
   props: {
     size: {
       default: 'normal',
