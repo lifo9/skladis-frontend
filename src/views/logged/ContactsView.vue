0ccc2311d5d3420685df1b5e95a5fb96f745ac33
Jakub Filo <jakub@filo.dev>
2021-12-29 14:38:59 +0100

    styling

--- src/views/logged/ContactsView.vue
+++ src/views/logged/ContactsView.vue
@@ -1,8 +1,6 @@
 <template>
   <div>
-    <h1
-      class="mt-4 mb-8 text-4xl font-bold leading-7 text-gray-900 sm:truncate"
-    >
+    <h1 class="mt-4 mb-8 text-4xl font-bold text-gray-900 sm:truncate">
       {{ $t('Contacts') }}
     </h1>
     <router-view />
