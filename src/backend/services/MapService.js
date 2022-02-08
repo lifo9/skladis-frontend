65f90685ea8d156ce61c40767a9dd3128d6a744b
Jakub Filo <jakub@filo.dev>
2022-02-08 22:28:18 +0100

    Use https for map service

--- src/backend/services/MapService.js
+++ src/backend/services/MapService.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 export function reverseGeoCode (longitude, lattitude) {
   return axios.get(
-    `http://nominatim.openstreetmap.org/reverse?format=json&lat=${lattitude}&lon=${longitude}&zoom=18&addressdetails=1`
+    `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lattitude}&lon=${longitude}&zoom=18&addressdetails=1`
   )
 }
