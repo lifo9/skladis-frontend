9ab106a4e30e55175b27cbc0fcff697326477911
Jakub Filo <jakub@filo.dev>
2022-02-20 20:02:40 +0100

    fix eventBus type error

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,17 @@
 /// <reference types="vite/client" />
 
+import { Emitter } from 'mitt'
+
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
   const component: DefineComponent<{}, {}, any>
   export default component
+}
+
+type EventType = string | symbol
+declare module '@vue/runtime-core' {
+  interface ComponentCustomProperties {
+    eventBus: Emitter<Record<EventType, unknown>>
+  }
 }
